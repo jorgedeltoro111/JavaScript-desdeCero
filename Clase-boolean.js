bf13f73868758aa04becdb5ab0de2eef07711504
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2024-04-16 12:21:36 -0600

    Metodos de conversion de booleanos y funcion

---
+++ Clase-boolean.js
@@ -0,0 +1,12 @@
+const isActive = true;
+
+const result = 5 > 3;
+console.log(result);
+//Nos retornara un true o un false dependiendo si la condicion es verdadera o falsa.
+//Para poder ver si una variable tiene un valor podemos utilizar los signos de admiracion 
+const number = 10;
+console.log(!!number);
+//conversion explicita - convertira el valor de value que es 0 y lo pondra en falso en la variable explicitValue.
+const value = 0;
+const explicitValue = Boolean(value);
+console.log(explicitValue);
